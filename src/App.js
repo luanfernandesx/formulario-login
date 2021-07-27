96d5250fcce8ed239f7d9adedcdbb425a04c5e2d
Luan Fernandes Oliveira <luan.oliveira@sequor.com.br>
2021-07-27 10:22:33 -0300

    initial-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+import './App.scss'
+
+const schema = yup.object().shape({
+  email: yup.string().email('O campo e-mail é inválido')
+   .required('O campo e-mail é obrigatorio!'),
+   senha: yup.string().required('O campo senha é obrigatório!')
+})
 
 function App() {
+
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema)
+  })
+
+    
+  function handleLogin({email,senha}) {
+    console.log(email,senha)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div className="container">
+       <form onSubmit={handleSubmit(handleLogin)} className="login-form">
+         <h3>Login</h3>
+         <div>
+           <label>E-mail</label>
+           <input 
+            type="text" 
+            name="email"
+            {...register('email')}
+            />
+            <span>{errors.email?.message}</span>
+          </div>
+          <div>
+            <label>Senha</label>
+            <input
+              type="password"
+              name="senha"
+              {...register('senha')}
+            />
+            <span>{errors.senha?.message}</span>
+          </div>
+          <button type="submit">Entrar</button>
+       </form>
+      </div> 
   );
 }
 
